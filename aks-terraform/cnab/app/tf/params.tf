cb573ad25ddd96291402385749e3f0d43ee748d1
vdice <vadice@microsoft.com>
2018-09-26 10:58:02 -0600

    feat(aks-terraform): add to aks-terraform bundle, including:
      * Automatic (Azure Storage Account-based) Terraform remote backend creation
      * Automatic CNAB_P_* to TF_VAR_* env var translation for use by Terraform
      * README.md

--- aks-terraform/cnab/app/tf/params.tf
+++ aks-terraform/cnab/app/tf/params.tf
@@ -1,26 +1,30 @@
 variable "client_id" {}
 variable "client_secret" {}
 
+variable "tenant_id" {}
+
+variable "subscription_id" {}
+
 variable "agent_count" {
-    default = 3
+    default = 1
 }
 
-variable "ssh_public_key" {
+variable "ssh_authorized_key" {
     default = "~/.ssh/id_rsa.pub"
 }
 
 variable "dns_prefix" {
-    default = "k8stest"
+    default = "akstest"
 }
 
-variable cluster_name {
-    default = "k8stest"
+variable "cluster_name" {
+    default = "akstest"
 }
 
-variable resource_group_name {
-    default = "azure-k8stest"
+variable "resource_group_name" {
+    default = "azure-akstest"
 }
 
 variable location {
-    default = "Central US"
+    default = "East US"
 }
